14ec53741aa62bc55808d5028aa8534a67d5c7de
exergasia <exergasia.expolitio.repetition@gmail.com>
2014-07-31 01:55:52 +0100

    Actually include the module

---
+++ Data/TaggedTree.hs
@@ -0,0 +1,110 @@
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric, DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+module Data.TaggedTree
+ ( TaggedTree(..)
+ , _TaggedTree
+ , subtree
+ , foldTaggedTree
+ , embedTaggedTree
+ , projectTaggedTree
+ , TaggedTreeF(..)
+ ) where
+
+import Data.Fenceposted
+import Data.Monoid
+import Control.Applicative
+import qualified Data.Foldable as F
+import Data.Traversable
+import Data.Bitraversable
+import Data.Bifoldable
+import Data.Semigroup.Bitraversable
+import Data.Semigroup.Bifoldable
+import Data.Functor.Apply
+import Data.Semigroup hiding ((<>))
+import qualified Data.Semigroup as Semi
+import Data.Semigroup.Foldable
+import Data.Semigroup.Traversable
+import Data.Bifunctor
+import qualified Data.Functor.Bind as Bind
+import GHC.Generics
+import Data.Data
+import Data.Profunctor
+
+data TaggedTree tag a
+      = TaggedTree { unTaggedTree :: Fenceposted a (tag, TaggedTree tag a) }
+    deriving (Eq, Ord, Read, Show, Generic, Data, Typeable)
+
+newtype TaggedTreeF tag a r = TaggedTreeF { unTaggedTreeF :: Fenceposted a (tag, r) }
+  deriving (Eq, Ord, Read, Show, Generic, Data, Typeable, Functor, F.Foldable, Traversable)
+
+tritraverse1TaggedTreeF :: (Apply f) => (tag -> f tag') -> (a -> f a') -> (r -> f r') -> TaggedTreeF tag a r -> f (TaggedTreeF tag' a' r')
+tritraverse1TaggedTreeF f g h = fmap TaggedTreeF . bitraverse1 g (bitraverse1 f h) . unTaggedTreeF
+
+bitraverse1TaggedTreeF :: (Apply f) => (a -> f a') -> (r -> f r') -> TaggedTreeF tag a r -> f (TaggedTreeF tag a' r')
+bitraverse1TaggedTreeF f g = fmap TaggedTreeF . bitraverse1 f (traverse1 g) . unTaggedTreeF
+
+-- | @_TaggedTree :: Iso (Fenceposted a (tag, TaggedTree tag a)) (Fenceposted a' (tag', TaggedTree tag' a')) (TaggedTree tag a) (TaggedTree tag' a')
+_TaggedTree :: (Profunctor p, Functor f) => p (TaggedTree tag a) (f (TaggedTree tag' a')) -> p (Fenceposted a (tag, TaggedTree tag a)) (f (Fenceposted a' (tag', TaggedTree tag' a')))
+_TaggedTree = dimap TaggedTree (fmap unTaggedTree)
+
+foldTaggedTree :: (TaggedTreeF tag a r -> r) -> TaggedTree tag a -> r
+foldTaggedTree f = f . fmap (foldTaggedTree f) . projectTaggedTree
+
+projectTaggedTree :: TaggedTree tag a -> TaggedTreeF tag a (TaggedTree tag a)
+projectTaggedTree = TaggedTreeF . unTaggedTree
+
+embedTaggedTree :: TaggedTreeF tag a (TaggedTree tag a) -> TaggedTree tag a
+embedTaggedTree = TaggedTree . unTaggedTreeF
+
+instance (Semigroup a) => Semigroup (TaggedTree tag a) where
+  a <> b = TaggedTree $ unTaggedTree a Semi.<> unTaggedTree b
+
+instance (Monoid a) => Monoid (TaggedTree tag a) where
+  mempty = TaggedTree mempty
+  mappend a b = fmap unwrapMonoid $ fmap WrapMonoid a Semi.<> fmap WrapMonoid b
+
+instance Bitraversable1 TaggedTree where
+  bitraverse1 f g = foldTaggedTree (fmap embedTaggedTree . tritraverse1TaggedTreeF f g id)
+
+instance Bifoldable1 TaggedTree where
+  bifoldMap1 = bifoldMap1Default
+
+instance Bitraversable TaggedTree where
+  bitraverse f g = unwrapApplicative . bitraverse1 (WrapApplicative . f) (WrapApplicative . g)
+
+instance Traversable1 (TaggedTree tag) where
+  traverse1 f = foldTaggedTree (fmap embedTaggedTree . bitraverse1TaggedTreeF f id)
+
+instance Foldable1 (TaggedTree tag) where
+  foldMap1 = foldMap1Default
+
+instance Traversable (TaggedTree tag) where
+  traverse = bitraverse pure
+
+instance Bifoldable TaggedTree where
+  bifoldMap = bifoldMapDefault
+
+instance F.Foldable (TaggedTree tag) where
+  foldMap = foldMapDefault
+
+instance Bifunctor TaggedTree where
+  bimap = bimapDefault
+
+instance Functor (TaggedTree tag) where
+  fmap = fmapDefault
+
+subtree :: (Monoid a) => tag -> TaggedTree tag a -> TaggedTree tag a
+subtree tag f = TaggedTree (pure (tag, f))
+
+instance Bind.Bind (TaggedTree tag) where
+  join = foldTaggedTree (TaggedTree . joinPosts . first unTaggedTree . unTaggedTreeF)
+
+instance Apply (TaggedTree tag) where
+  f <.> a = Bind.join $ (<$> a) <$> f
+
+instance Applicative (TaggedTree tag) where
+  pure = TaggedTree . fencepost
+  (<*>) = (<.>)
+
+instance Monad (TaggedTree tag) where
+  return = pure
+  a >>= f = Bind.join $ f <$> a
