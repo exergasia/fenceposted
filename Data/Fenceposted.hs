fdc65a0b0706a3b7ace93ce5b2893652be9a461b
exergasia <exergasia.expolitio.repetition@gmail.com>
2014-09-08 10:09:00 +0100

    Inline them all, let GHC sort them out.

--- Data/Fenceposted.hs
+++ Data/Fenceposted.hs
@@ -45,47 +45,60 @@ data Fenceposted post a = Fenceposted [(post, a)] post
 
 instance (Eq post) => Eq1 (Fenceposted post) where
   eq1 = (==)
+  {-# INLINE eq1 #-}
 
 instance (Ord post) => Ord1 (Fenceposted post) where
   compare1 = compare
+  {-# INLINE compare1 #-}
 
 instance (Read post) => Read1 (Fenceposted post) where
   readsPrec1 = readsPrec
+  {-# INLINE readsPrec1 #-}
 
 instance (Show post) => Show1 (Fenceposted post) where
   showsPrec1 = showsPrec
+  {-# INLINE showsPrec1 #-}
 
 -- | @posts :: Traversal1 (Fenceposted post a) (Fenceposted post\' a) post post\'@
 posts :: (Apply f) => (post -> f post') -> Fenceposted post a -> f (Fenceposted post' a)
 posts f (Fenceposted xs z) = F.foldr (\ (post, x) acc -> flip panel x <$> f post <.> acc) (fencepost <$> f z) xs
+{-# INLINE posts #-}
 
 -- | A single terminal fencepost.
 fencepost :: post -> Fenceposted post a
 fencepost = Fenceposted mempty
+{-# INLINE fencepost #-}
 
 -- | Add a new \'post\' and a \'panel\' at the left.
 panel :: post -> a -> Fenceposted post a -> Fenceposted post a
 panel post x (Fenceposted xs z) = Fenceposted ((post, x) : xs) z
+{-# INLINE panel #-}
 
 joinPosts :: Fenceposted (Fenceposted post a) a -> Fenceposted post a
 joinPosts (Fenceposted xs z) = foldr f z xs
   where
     f (Fenceposted as az, x) (Fenceposted bs bz) = Fenceposted (as <> ((az, x):bs)) bz
+{-# INLINE joinPosts #-}
 
 instance Bitraversable1 Fenceposted where
   bitraverse1 f g (Fenceposted xs z) = F.foldr (liftF2 (uncurry panel)) (fencepost <$> f z) $ bitraverse1 f g <$> xs
+  {-# INLINE bitraverse1 #-}
 
 instance Bitraversable Fenceposted where
   bitraverse f g = unwrapApplicative . bitraverse1 (WrapApplicative . f) (WrapApplicative . g)
+  {-# INLINE bitraverse #-}
 
 instance Bifoldable1 Fenceposted where
   bifoldMap1 = bifoldMap1Default
+  {-# INLINE bifoldMap1 #-}
 
 instance Bifoldable Fenceposted where
   bifoldMap = bifoldMapDefault
+  {-# INLINE bifoldMap #-}
 
 instance Bifunctor Fenceposted where
   bimap = bimapDefault
+  {-# INLINE bimap #-}
 
 instance (Semigroup post) => Semigroup (Fenceposted post a) where
   Fenceposted as aEnd <> b =
@@ -96,34 +109,46 @@ instance (Semigroup post) => Semigroup (Fenceposted post a) where
 
 instance (Monoid post) => Monoid (Fenceposted post a) where
   mempty = fencepost mempty
+  {-# INLINE mempty #-}
   mappend a b = first unwrapMonoid $ first WrapMonoid a Semi.<> first WrapMonoid b
   {-# INLINE mappend #-}
 
 -- | A \'productish\' instance.
 instance (Semigroup post) => Apply (Fenceposted post) where
   f <.> a = Bind.join $ (<$> a) <$> f
+  {-# INLINE (<.>) #-}
 
 instance (Monoid post) => Applicative (Fenceposted post) where
   pure x = Fenceposted (pure (mempty, x)) mempty
+  {-# INLINE pure #-}
   f <*> a = first unwrapMonoid $ first WrapMonoid f <.> first WrapMonoid a
+  {-# INLINE (<*>) #-}
 
 instance (Semigroup post) => Bind.Bind (Fenceposted post) where
   join = bifoldMap1 fencepost id
+  {-# INLINE join #-}
 
 instance (Monoid post) => Monad (Fenceposted post) where
   return = pure
+  {-# INLINE return #-}
   a >>= f = first unwrapMonoid $ Bind.join $ fmap (first WrapMonoid . f) (first WrapMonoid a)
+  {-# INLINE (>>=) #-}
 
 instance (Semigroup post) => Alt (Fenceposted post) where
   (<!>) = (Semi.<>)
+  {-# INLINE (<!>) #-}
 
 instance (Monoid post) => Alternative (Fenceposted post) where
   empty = mempty
+  {-# INLINE empty #-}
   (<|>) = mappend
+  {-# INLINE (<|>) #-}
 
 instance (Monoid post) => MonadPlus (Fenceposted post) where
   mzero = mempty
+  {-# INLINE mzero #-}
   mplus = mappend
+  {-# INLINE mplus #-}
 
 -- | Base functor for @'Fenceposted'@.
 data FencepostedF post a r
@@ -133,25 +158,32 @@ data FencepostedF post a r
 
 instance (Eq post, Eq a) => Eq1 (FencepostedF post a) where
   eq1 = (==)
+  {-# INLINE eq1 #-}
 
 instance (Ord post, Ord a) => Ord1 (FencepostedF post a) where
   compare1 = compare
+  {-# INLINE compare1 #-}
 
 instance (Read post, Read a) => Read1 (FencepostedF post a) where
   readsPrec1 = readsPrec
+  {-# INLINE readsPrec1 #-}
 
 instance (Show post, Show a) => Show1 (FencepostedF post a) where
   showsPrec1 = showsPrec
+  {-# INLINE showsPrec1 #-}
 
 fencepostedF :: (post -> x) -> (post -> a -> r -> x) -> FencepostedF post a r -> x
 fencepostedF f _ (FinalPost post) = f post
 fencepostedF _ g (Panel post a r) = g post a r
+{-# INLINE fencepostedF #-}
 
 tritraverse1FencepostedF :: (Apply f) => (post -> f post') -> (a -> f a') -> (r -> f r') -> FencepostedF post a r -> f (FencepostedF post' a' r')
 tritraverse1FencepostedF f g h = fencepostedF (fmap FinalPost . f) (\ post a r -> Panel <$> f post <.> g a <.> h r)
+{-# INLINE tritraverse1FencepostedF #-}
 
 embedFenceposted :: FencepostedF post a (Fenceposted post a) -> Fenceposted post a
 embedFenceposted = fencepostedF fencepost panel
+{-# INLINE embedFenceposted #-}
 
 projectFenceposted :: Fenceposted post a -> FencepostedF post a (Fenceposted post a)
 projectFenceposted (Fenceposted xs z) =
@@ -166,37 +198,49 @@ newtype ZipFenceposted post a = ZipFenceposted { getZipFenceposted :: Fenceposte
 
 instance (Eq post) => Eq1 (ZipFenceposted post) where
   eq1 = (==)
+  {-# INLINE eq1 #-}
 
 instance (Ord post) => Ord1 (ZipFenceposted post) where
   compare1 = compare
+  {-# INLINE compare1 #-}
 
 instance (Read post) => Read1 (ZipFenceposted post) where
   readsPrec1 = readsPrec
+  {-# INLINE readsPrec1 #-}
 
 instance (Show post) => Show1 (ZipFenceposted post) where
   showsPrec1 = showsPrec
+  {-# INLINE showsPrec1 #-}
 
 instance Bitraversable1 ZipFenceposted where
   bitraverse1 f g = fmap ZipFenceposted . bitraverse1 f g . getZipFenceposted
+  {-# INLINE bitraverse1 #-}
 
 instance Bitraversable ZipFenceposted where
   bitraverse f g = fmap ZipFenceposted . bitraverse f g . getZipFenceposted
+  {-# INLINE bitraverse #-}
 
 instance Bifoldable ZipFenceposted where
   bifoldMap = bifoldMapDefault
+  {-# INLINE bifoldMap #-}
 
 instance Bifoldable1 ZipFenceposted where
   bifoldMap1 f g = bifoldMap1 f g . getZipFenceposted
+  {-# INLINE bifoldMap1 #-}
 
 instance Bifunctor ZipFenceposted where
   bimap = bimapDefault
+  {-# INLINE bimap #-}
 
 instance (Semigroup post) => Semigroup (ZipFenceposted post a) where
   ZipFenceposted a <> ZipFenceposted b = ZipFenceposted $ a Semi.<> b
+  {-# INLINE (Semi.<>) #-}
 
 instance (Monoid post) => Monoid (ZipFenceposted post a) where
   mempty = ZipFenceposted mempty
+  {-# INLINE mempty #-}
   mappend (ZipFenceposted a) (ZipFenceposted b) = ZipFenceposted $ mappend a b
+  {-# INLINE mappend #-}
 
 -- | Zip together two @Fenceposted@s with the given combining functions.
 fencepostZipWith :: (p -> q -> r) -> (a -> b -> c) -> Fenceposted p a -> Fenceposted q b -> Fenceposted r c
@@ -206,13 +250,18 @@ fencepostZipWith f g a b =
     (FinalPost aPost, Panel bPost _ _) -> fencepost (f aPost bPost)
     (Panel aPost _ _, FinalPost bPost) -> fencepost (f aPost bPost)
     (Panel aPost aVal as, Panel bPost bVal bs) -> panel (f aPost bPost) (g aVal bVal) $ fencepostZipWith f g as bs
+{-# INLINE fencepostZipWith #-}
 
 fencepostRepeat :: (Monoid post) => a -> Fenceposted post a
 fencepostRepeat x = panel mempty x $ fencepostRepeat x
+{-# INLINE fencepostRepeat #-}
 
 instance (Semigroup post) => Apply (ZipFenceposted post) where
   ZipFenceposted a <.> ZipFenceposted b = ZipFenceposted $ fencepostZipWith (Semi.<>) ($) a b
+  {-# INLINE (<.>) #-}
 
 instance (Monoid post) => Applicative (ZipFenceposted post) where
   pure = ZipFenceposted . fencepostRepeat
+  {-# INLINE pure #-}
   a <*> b = first unwrapMonoid $ first WrapMonoid a <.> first WrapMonoid b
+  {-# INLINE (<*>) #-}
--- Data/TaggedTree.hs
+++ Data/TaggedTree.hs
@@ -36,21 +36,26 @@ data TaggedTree tag a
 -- | @_TaggedTree :: Iso (Fenceposted a (tag, TaggedTree tag a)) (Fenceposted a' (tag', TaggedTree tag' a')) (TaggedTree tag a) (TaggedTree tag' a')
 _TaggedTree :: (Profunctor p, Functor f) => p (TaggedTree tag a) (f (TaggedTree tag' a')) -> p (Fenceposted a (tag, TaggedTree tag a)) (f (Fenceposted a' (tag', TaggedTree tag' a')))
 _TaggedTree = dimap TaggedTree (fmap unTaggedTree)
+{-# INLINE _TaggedTree #-}
 
 foldTaggedTree :: (Fenceposted a (tag, r) -> r) -> TaggedTree tag a -> r
 foldTaggedTree f = f . fmap (fmap (foldTaggedTree f)) . projectTaggedTree
+{-# INLINE foldTaggedTree #-}
 
 projectTaggedTree :: TaggedTree tag a -> Fenceposted a (tag, TaggedTree tag a)
 projectTaggedTree = unTaggedTree
+{-# INLINE projectTaggedTree #-}
 
 embedTaggedTree :: Fenceposted a (tag, TaggedTree tag a) -> TaggedTree tag a
 embedTaggedTree = TaggedTree
+{-# INLINE embedTaggedTree #-}
 
 nullTaggedTree :: (Eq a, Monoid a) => TaggedTree tag a -> Bool
 nullTaggedTree t =
   case projectFenceposted $ unTaggedTree t of
     FinalPost x -> x == mempty
     Panel _ _ _ -> False
+{-# INLINE nullTaggedTree #-}
 
 instance (Semigroup a) => Semigroup (TaggedTree tag a) where
   a <> b = TaggedTree $ unTaggedTree a Semi.<> unTaggedTree b
@@ -58,52 +63,70 @@ instance (Semigroup a) => Semigroup (TaggedTree tag a) where
 
 instance (Monoid a) => Monoid (TaggedTree tag a) where
   mempty = TaggedTree mempty
+  {-# INLINE mempty #-}
   mappend a b = fmap unwrapMonoid $ fmap WrapMonoid a Semi.<> fmap WrapMonoid b
   {-# INLINE mappend #-}
 
 instance Bitraversable1 TaggedTree where
   bitraverse1 f g = foldTaggedTree (fmap embedTaggedTree . bitraverse1 g (bitraverse1 f id))
+  {-# INLINE bitraverse1 #-}
 
 instance Bifoldable1 TaggedTree where
   bifoldMap1 = bifoldMap1Default
+  {-# INLINE bifoldMap1 #-}
 
 instance Bitraversable TaggedTree where
   bitraverse f g = unwrapApplicative . bitraverse1 (WrapApplicative . f) (WrapApplicative . g)
+  {-# INLINE bitraverse #-}
 
 instance Traversable1 (TaggedTree tag) where
   traverse1 f = foldTaggedTree (fmap embedTaggedTree . bitraverse1 f sequence1)
+  {-# INLINE traverse1 #-}
 
 instance Foldable1 (TaggedTree tag) where
   foldMap1 = foldMap1Default
+  {-# INLINE foldMap1 #-}
 
 instance Traversable (TaggedTree tag) where
   traverse = bitraverse pure
+  {-# INLINE traverse #-}
 
 instance Bifoldable TaggedTree where
   bifoldMap = bifoldMapDefault
+  {-# INLINE bifoldMap #-}
 
 instance F.Foldable (TaggedTree tag) where
   foldMap = foldMapDefault
+  {-# INLINE foldMap #-}
 
 instance Bifunctor TaggedTree where
   bimap = bimapDefault
+  {-# INLINE bimap #-}
 
 instance Functor (TaggedTree tag) where
   fmap = fmapDefault
+  {-# INLINE fmap #-}
 
 subtree :: (Monoid a) => tag -> TaggedTree tag a -> TaggedTree tag a
 subtree tag f = TaggedTree (pure (tag, f))
+{-# INLINE subtree #-}
 
 instance Bind.Bind (TaggedTree tag) where
   join = foldTaggedTree (TaggedTree . joinPosts . first unTaggedTree)
+  {-# INLINE join #-}
 
 instance Apply (TaggedTree tag) where
   f <.> a = Bind.join $ (<$> a) <$> f
+  {-# INLINE (<.>) #-}
 
 instance Applicative (TaggedTree tag) where
   pure = TaggedTree . fencepost
+  {-# INLINE pure #-}
   (<*>) = (<.>)
+  {-# INLINE (<*>) #-}
 
 instance Monad (TaggedTree tag) where
   return = pure
+  {-# INLINE return #-}
   a >>= f = Bind.join $ f <$> a
+  {-# INLINE (>>=) #-}
